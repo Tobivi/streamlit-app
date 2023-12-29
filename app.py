2ddcb6356bee98927751ece430718c076de4be4e
Tobivictor <108303792+Tobivi@users.noreply.github.com>
2023-12-29 01:11:21 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st 
+from PIL import Image
+
+st.title("Apology message to my woman(Abimbola Mercy Oluwatoba)")
+    
+st.write("Kindly watch this video first babe before reading the content")
+
+
+video_file = open('Untitled video - Made with Clipchamp - Trim.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+
+st.write("""
+         
+Dear Babe,
+
+I hope this message reaches you in a moment when you can give it the time and consideration it deserves. I've spent a lot of time reflecting on recent events, and I believe it's crucial for me to express my feelings and thoughts in a more detailed manner. Please bear with me as I attempt to convey the depth of my emotions and my sincere apology for any pain I may have caused.
+
+I want to start by acknowledging that I have let you down, and I am truly sorry for that. I am aware that my recent actions have hurt you, and seeing you upset is the last thing I ever wanted. It pains me to think that I have been the source of any distress in your life, and I want to make things right.
+
+As I reflect on the recent events, I recognize that my behavior has fallen short of the standards that a loving and supportive partner should uphold. I have let stress, frustration, and other external factors affect our relationship, and in doing so, I failed to prioritize the most important person in my life—you.
+
+It's difficult to put into words the regret and remorse I feel for the times I made you feel unimportant or neglected. I understand the weight of my actions, and I want you to know that I am not making excuses. Instead, I am taking full responsibility for my behavior, acknowledging the impact it had on you, and expressing a sincere desire to make amends.
+
+Relationships are built on trust, communication, and mutual respect. In recent times, I realize I've fallen short in maintaining these foundations. I failed to communicate effectively, and I allowed external pressures to affect the way I treated you. I understand that this is not fair to you, and I am committed to addressing these issues head-on.
+
+I want you to know that I value and appreciate you more than words can express. You are an integral part of my life, and the thought of losing you is devastating. Your happiness and well-being are of the utmost importance to me, and I want to do everything in my power to ensure that our relationship is a source of joy and support for both of us.
+
+In the spirit of transparency and self-reflection, I want to share some insights into what led to my recent behavior. This is not to make excuses but to provide you with a better understanding of where I was coming from. I believe that open communication is essential for growth and improvement in any relationship.
+
+Over the past few weeks, I have been dealing with some personal challenges. While this is not an excuse for my actions, I want you to understand the context in which I was operating. However, I realize that I should have communicated my struggles with you and sought your support rather than allowing them to affect our relationship negatively.
+
+I am committed to making changes in my behavior and addressing the root causes of my recent actions. This includes seeking professional help, if necessary, to develop healthier coping mechanisms and communication skills. I want to be the partner you deserve, and I am willing to put in the effort to make that a reality.
+
+Moving forward, I am dedicated to creating a more positive and nurturing environment for our relationship. This involves prioritizing quality time together, actively listening to your needs and concerns, and working together to overcome any challenges that may arise. I want our relationship to be a source of strength and joy for both of us.
+
+I understand that actions speak louder than words, and I am committed to demonstrating through my behavior that I am serious about positive change. I am open to any discussions or suggestions you may have regarding how we can move forward together. Your feelings and needs are important to me, and I want to ensure that we are both actively contributing to the growth and well-being of our relationship.
+
+I recognize that trust may have been compromised, and I am willing to be patient and consistent in rebuilding it. I understand that rebuilding trust is a process that takes time, and I am prepared to demonstrate through my actions that I am committed to earning back your trust.
+
+I also want to address any emotional pain or stress that my actions may have caused you. Your feelings are valid, and I am here to support you in any way you need. If you would like to discuss your feelings further or if there are specific concerns you would like me to address, I am ready to listen with an open heart.
+
+In conclusion, I want to reiterate how deeply sorry I am for any pain I've caused you. I value our relationship immensely, and I am committed to doing the necessary work to make things right. I appreciate your understanding and patience as we navigate through this challenging time.
+
+Thank you for taking the time to read this message. I am here for you, and I hope we can work together to rebuild and strengthen our relationship.
+
+With sincere apologies and a commitment to positive change.
+
+I LOVE YOU BABE \u2764\uFE0F""") 
+
+st.balloons()
+
